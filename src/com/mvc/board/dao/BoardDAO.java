55290eaefc9c56af0bc81f48f19cd08381dd3ce6
seokgieMin <mings805@naver.com>
2021-08-09 20:59:01 +0900

    d

--- src/com/mvc/board/dao/BoardDAO.java
+++ src/com/mvc/board/dao/BoardDAO.java
@@ -448,30 +448,6 @@ public class BoardDAO {
 		return success;
 	}
 
-<<<<<<< HEAD
-	public int like(String contentNo, String email, String likeCnt, String footPrintNo) {
-		int success =0;
-		String sql1="INSERT INTO LIKES(contentNO, email , likeCnt)VALUES(?,?,1)";
-		String sql2 ="UPDATE footprint SET  likeCnt = likeCnt - 1 WHERE footPrintNO =?";
-		
-		try {
-			ps = conn.prepareStatement(sql1);
-			ps.setString(1, contentNo);
-			ps.setString(2, email);
-		
-			ps.executeUpdate();
-			rs = ps.getGeneratedKeys();
-			if(rs.next()) {
-				ps.setString(1, footPrintNo);
-				ps.executeUpdate();
-				success = ps.executeUpdate();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		return success;
-=======
 	public void like(String fpn, String email) {
 		String sql1 = "select likecnt from likes where contentno=? AND email=?";
 		String sql2 = "INSERT  INTO likes(contentno, likecnt, email) VALUES(?,1,?)";
@@ -520,7 +496,6 @@ public class BoardDAO {
 		} finally {
 			resClose();
 		}
->>>>>>> 820f472e92302fc582061b1eac317951d2e0e48f
 	}
 
 	
