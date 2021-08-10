815211a40c2fc07a989d38d0583a3a4de536af8e
seokgieMin <mings805@naver.com>
2021-08-10 15:16:38 +0900

    merge

--- src/com/mvc/board/controller/BoardController.java
+++ src/com/mvc/board/controller/BoardController.java
@@ -17,7 +17,6 @@ import com.oreilly.servlet.MultipartRequest;
 @WebServlet({"/like","/fpsearch","/fplist","/fpwriteOk","/fpwriteNo","/fpdetail","/fpdel","/fpupdateForm","/fpupdate","/fpserach","/feedlist","/fdReport","/fdReportWrite"})
 
 
-
 public class BoardController extends HttpServlet {
 
 
@@ -40,8 +39,6 @@ public class BoardController extends HttpServlet {
 
 	private void dual(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException{
-	
-		
 		
 		String uri = req.getRequestURI();
 		String ctx = req.getContextPath();
@@ -61,8 +58,8 @@ public class BoardController extends HttpServlet {
 			  String email = (String) req.getSession().getAttribute("loginemail");
 			  req.setAttribute("fplist",  service.fplist(email));
 			  dis = req.getRequestDispatcher("fplist.jsp");
-		  dis.forward(req, resp); 
-		  break;
+			  dis.forward(req, resp); 
+			  break;
 		  
 		  case "/feedlist":
 			  System.out.println("피드 불러오기");
@@ -175,8 +172,8 @@ public class BoardController extends HttpServlet {
 			dis = req.getRequestDispatcher("/feedlist");
 			dis.forward(req, resp);
 			break;
-		
-		  case "/like": 
+
+		case "/like": 
 			  boolean suc;
 			  System.out.println("조아용"); 
 			  email = (String)  req.getSession().getAttribute("loginemail"); 
@@ -186,10 +183,6 @@ public class BoardController extends HttpServlet {
 			  dis = req.getRequestDispatcher("/feedlist");
 			  dis.forward(req, resp);
 		  break;
-
-		 
 		}		
-
-
 	}
 }
